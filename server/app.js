3df784bf295670e4fad64bc55d0836eb7f64cc39
Your Name <you@example.com>
2018-04-28 15:41:55 +0100

    Create APIs to dislay and create a menu for a specific day

--- server/app.js
+++ server/app.js
@@ -1,11 +1,11 @@
 import express from 'express';
 import bodyParser from 'body-parser';
-import meals from './routes/meals';
+import menu from './routes/menu';
 
 const app = express();
 app.use(express.json());
 app.use(bodyParser.text());
-app.use('/api/v1/meals', meals);
+app.use('/api/v1/menu', menu);
 
 const port = process.env.PORT || 3000;
 app.listen(port);
