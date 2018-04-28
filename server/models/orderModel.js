318a5bdf3d863da8130e2af5ca3bc62f8863352d
Your Name <you@example.com>
2018-04-28 21:14:24 +0100

    Test travis

--- server/models/orderModel.js
+++ server/models/orderModel.js
@@ -2,6 +2,8 @@ const orders = [
   { id: 1, meal: 'Rice with beef', quantity: 2 },
   { id: 2, meal: 'Rice with chicken', quantity: 3 },
   { id: 3, meal: 'Rice with plantain', quantity: 1 },
+  { id: 4, meal: 'Rice with stew', quantity: 1 },
+  { id: 5, meal: 'Rice with turkey', quantity: 1 },
 ];
 
 export default orders;
