393a8ca22dfe6617dd1089a3b97e15281ae755f3
Tobi Adelaja <tobio.adelaja@gmail.com>
2018-04-29 11:39:29 +0100

    Edit app.js

--- server/app.js
+++ server/app.js
@@ -5,7 +5,7 @@ import menu from './routes/menu';
 const app = express();
 app.use(express.json());
 app.use(bodyParser.text());
-app.use('/api/v1/meals', menu);
+app.use('/api/v1/menu', menu);
 
 const port = process.env.PORT || 3000;
 app.listen(port);
