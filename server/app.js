42e023c59447478c532253c0cb2903dde000fa9c
Your Name <you@example.com>
2018-04-29 21:45:00 +0100

    Add menu and meal features

--- server/app.js
+++ server/app.js
@@ -1,10 +1,14 @@
 import express from 'express';
 import bodyParser from 'body-parser';
 import order from './routes/order';
+import meals from './routes/meals';
+import menu from './routes/menu';
 
 const app = express();
 app.use(express.json());
 app.use(bodyParser.text());
+app.use('/api/v1/meals', meals);
+app.use('/api/v1/menu', menu);
 app.use('/api/v1/orders', order);
 
 const port = process.env.PORT || 3000;
