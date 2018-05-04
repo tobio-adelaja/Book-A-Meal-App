935b1ab12245dca48ef416a682e7a955f6daf35c
Tobi Adelaja <tobio.adelaja@gmail.com>
2018-05-04 14:44:46 +0100

    Refactor codebase for better efficiency (#9)
    Refactor codebase for better efficiency

--- server/app.js
+++ server/app.js
@@ -2,18 +2,14 @@ import express from 'express';
 
 import bodyParser from 'body-parser';
 
-import order from './routes/order';
-
-import meals from './routes/meals';
+import routes from './routes/routes';
 
 import menu from './routes/menu';
 
 const app = express();
 app.use(express.json());
 app.use(bodyParser.text());
-app.use('/api/v1/meals', meals);
-app.use('/api/v1/menu', menu);
-app.use('/api/v1/orders', order);
+app.use('/api/v1', routes);
 
 const port = process.env.PORT || 3000;
 app.listen(port);
